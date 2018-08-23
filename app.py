0e5f328c36cba7f80470fb7b387f9d8317f28a5b
vikasGhildiyal <41319365+vikasGhildiyal@users.noreply.github.com>
2018-08-23 22:38:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def makeWebhookResult():
     }
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-    app.run(debug=False, port=port, host='127.0.0.1')
+    app.run(debug=False, port=port, host='0.0.0.0')
